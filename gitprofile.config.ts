5dc37eecd3c8168c09e2c6ac8240ebe5a79140a2
Joel-Sousa <softmock@gmail.com>
2024-10-26 19:56:20 -0300

    Ajuste

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,14 +2,14 @@
 
 const CONFIG = {
   github: {
-    username: 'knight7561', // Your GitHub org/user name. (This is the only required config)
+    username: 'joel-sousa', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/',
+  base: '/joel-sousa/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
@@ -20,156 +20,162 @@ const CONFIG = {
         limit: 8, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
-          projects: ["Knight7561/WeatherApp","Knight7561/linkedIn_auto_jobs_applier_with_AI"], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
-      },
-      manual: {
-        // Properties for manually specifying projects
-        projects: [], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
       header: 'My Projects',
       // To hide the `External Projects` section, keep it empty.
       projects: [
-        // {
-        //   title: 'Project Name',
-        //   description:
-        //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-        //   imageUrl:
-        //     'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-        //   link: 'https://example.com',
-        // },
-        // {
-        //   title: 'Project Name',
-        //   description:
-        //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-        //   imageUrl:
-        //     'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-        //   link: 'https://example.com',
-        // },
+        {
+          title: 'Project Name',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          link: 'https://example.com',
+        },
+        {
+          title: 'Project Name',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          link: 'https://example.com',
+        },
+        {
+          title: 'Project Name',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          link: 'https://example.com',
+        },
+        {
+          title: 'Project Name',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          link: 'https://example.com',
+        },
       ],
     },
   },
   seo: {
-    title: 'Portfolio of Rohith Venkata Reddy',
+    title: 'Portfolio Joel Sousa',
     description: '',
     imageURL: '',
   },
   social: {
-    linkedin: 'rohithvr3',
-    twitter: 'octolearn',
-    mastodon: 'distributedTerminal@mastodon.social',
+    linkedin: 'joelsousa95',
+    twitter: '',
+    mastodon: '',
     researchGate: '',
     facebook: '',
     instagram: '',
     reddit: '',
     threads: '',
     youtube: '', // example: 'pewdiepie'
-    udemy: '',
+    udemy: 'joel-sousa-5',
     dribbble: '',
     behance: '',
-    medium: 'rohithvr3',
+    medium: '',
     dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://knight7561.github.io',
+    website: '',
     phone: '',
-    email: 'rohithvr3@gmail.com',
+    email: 'joelfsousa95@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/uc?export=download&id=1Bg29oa3xOEQLps1_5Vt72jfOh9aNBb4j', // Empty fileUrl will hide the `Download Resume` button.
+      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'Python',
-    'Tensorflow',
-    'SQL',
+    'Laravel',
+    'PHP',
+    'React.js',
+    'TypeScript',
+    'JavaScript',
+    'Filament',
+    'Node.js',
+    '',
+    'MySQL',
+    'PostgreSQL',
     'Git',
     'Docker',
-    'NLTK',
-    'SpaCy',
-    'LangChain',
-    'Pandas',
-    'RAG'
+    'PHPUnit',
+    'CSS',
+    'Antd',
+    'Tailwind',
   ],
   experiences: [
     {
-      company: 'Acunor',
-      position: 'AI Engineer',
-      from: 'May 2024',
-      to: 'September 2024',
-      companyLink: 'https://www.acunor.com',
+      company: 'Company Name',
+      position: 'Position',
+      from: 'September 2021',
+      to: 'Present',
+      companyLink: 'https://example.com',
     },
     {
-      company: 'Cyberinfrastructure for Network Science (CNS) Center',
-      position: 'Software Developer, Data',
-      from: 'May 2022',
-      to: 'September 2023',
-      companyLink: 'https://cns.iu.edu/home.html',
-    },
-    {
-      company: 'NEC',
-      position: 'Software Developer, ML',
-      from: 'Aug 2019',
-      to: 'Juky 2021',
-      companyLink: 'https://in.nec.com',
+      company: 'Company Name',
+      position: 'Position',
+      from: 'July 2019',
+      to: 'August 2021',
+      companyLink: 'https://example.com',
     },
   ],
   certifications: [
     {
-      name: 'AWS Certified Machine Learning - Specialty',
-      body: 'Amazon Web Services - MLS-C01',
-      year: 'January 2024',
-      link: 'https://www.credly.com/badges/f6b504e7-89ee-4689-bcd2-aead62d82932/public_url',
+      name: 'Lorem ipsum',
+      body: 'Lorem ipsum dolor sit amet',
+      year: 'March 2022',
+      link: 'https://example.com',
     },
-    {
-      name: 'Databricks Certified Data Engineer Associate',
-      body: 'Databricks',
-      year: 'December 2023',
-      link: 'https://credentials.databricks.com/ad5ce0c6-5b35-4589-9312-27a12ad8468e',
-    },
-    {
-      name: 'Google Data Analytics Specialization',
-      body: 'Google',
-      year: 'January 2022',
-      link: 'https://www.coursera.org/account/accomplishments/professional-cert/5AFRK8EF3BNZ',
-    },
-
-
   ],
   educations: [
     {
-      institution: 'Indiana University Bloomington',
-      degree: 'Master of Science in Data Science',
-      from: '2021',
-      to: '2023',
+      institution: 'Institution Name',
+      degree: 'Degree',
+      from: '2015',
+      to: '2019',
     },
     {
-      institution: 'Visvesvaraya Technological University',
-      degree: 'Undergraduate - Bachelor in Computer Science and Engineering',
-      from: '2016',
-      to: '2020',
+      institution: 'Institution Name',
+      degree: 'Degree',
+      from: '2012',
+      to: '2014',
     },
   ],
-  publications: [
-    {
-      title: 'Recommendation and Product Analyzer System for E-Commerce',
-      conferenceName: '',
-      journalName: 'International Research Journal of Engineering and Technology (IRJET) ',
-      authors: 'Ramya Shree B, Nama Venkata Naga Sukesh, V.R Rohith, Pranav N Rao, Sajitha N',
-      link: 'https://www.irjet.net/archives/V7/i6/IRJET-V7I6584.pdf',
-      description:
-        'The recommendation system derives a relationship between the products and users to help them to select the most appropriate product and make user decision process faster. This is achieved by using collaborative filtering. This system builds a model that enhances the seller experience by providing the positive and negative reviews given by user directly using sentimental analyzer which makes use of LSTM model for sentiment analysis for better results.',
-    }
-  ],
+  // publications: [
+  //   {
+  //     title: 'Publication Title',
+  //     conferenceName: '',
+  //     journalName: 'Journal Name',
+  //     authors: 'John Doe, Jane Smith',
+  //     link: 'https://example.com',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  //   },
+  //   {
+  //     title: 'Publication Title',
+  //     conferenceName: 'Conference Name',
+  //     journalName: '',
+  //     authors: 'John Doe, Jane Smith',
+  //     link: 'https://example.com',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  //   },
+  // ],
   // Display articles from your medium or dev account. (Optional)
-  blog: {
-    source: 'medium', // medium | dev
-    username: 'rohithvr3', // to hide blog section, keep it empty
-    limit: 3, // How many articles to display. Max is 10.
-  },
+  // blog: {
+  //   source: 'dev', // medium | dev
+  //   username: 'joel-sousa', // to hide blog section, keep it empty
+  //   limit: 2, // How many articles to display. Max is 10.
+  // },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
@@ -242,16 +248,6 @@ const CONFIG = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a
-      class="text-primary" href=""
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> (Major credits) and ❤️.
-    <a
-      class="text-primary" href="https://sites.google.com/view/rohithvenkatareddy#h.m2wub9pekwzn"
-      target="_blank"
-      rel="noreferrer"
-    >Meet me here</a>`,
 
   enablePWA: true,
 };
